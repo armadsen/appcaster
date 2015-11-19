e027c97d97cb4a5d5bff9bbec4a5c6f1ab9be62b
Andrew Madsen <andrew@openreelsoftware.com>
2015-11-18 19:15:13 -0700

    Fixed possible extra slash in link to release notes and download in appcasts.

--- helpers/index.js
+++ helpers/index.js
@@ -1,17 +1,19 @@
 module.exports = {
   bootstrap: require('./bootstrap'),
 
-  buildNotesUrl: function(urlRoot, appcast, build) {
-    var url = '/apps/:url_slug/:channel_url_slug/release-notes/:version.html';
+  buildNotesUrl: function(rootUrl, appcast, build) {
+    var url = 'apps/:url_slug/:channel_url_slug/release-notes/:version.html';
     url = url.replace(/:url_slug/, appcast.app.url_slug);
     url = url.replace(/:channel_url_slug/, appcast.channel.url_slug);
     url = url.replace(/:version/, build.version);
 
-    return urlRoot + url;
+	if (rootUrl.substr(-1) != '/') rootUrl += '/';
+    return rootUrl + url;
   },
 
   downloadUrl: function(rootUrl, build, channel) {
-    var template = rootUrl + '/apps/:url_slug/:channel_url_slug/download/:version/:filename';
+	if (rootUrl.substr(-1) != '/') rootUrl += '/';
+    var template = rootUrl + 'apps/:url_slug/:channel_url_slug/download/:version/:filename';
     template = template.replace(/:url_slug/, channel.app_url_slug);
     template = template.replace(/:channel_url_slug/, channel.url_slug);
     template = template.replace(/:version/, build.version);
