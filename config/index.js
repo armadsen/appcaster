b14de0b0d5f92694b3777939c17adb9a82154260
Andrew Madsen <andrew@openreelsoftware.com>
2021-03-17 20:46:26 +0000

    Update ejs include syntax

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 var env = process.env.NODE_ENV || "development";
-process.env.AC_GITHUB_USERS = process.env.AC_GITHUB_USERS || ["alexyoung"];
+process.env.AC_GITHUB_USERS = process.env.AC_GITHUB_USERS || ["armadsen"];
 
 function parseGitHubUsers(users) {
   return users.split(",").map(function (username) {
@@ -8,12 +8,19 @@ function parseGitHubUsers(users) {
   });
 }
 
-var database = {
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-};
+var database;
+if (env === "development") {
+  database = {
+    connectionString: process.env.DATABASE_URL,
+  };
+} else {
+  database = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+}
 
 // Use environmental variables by default
 module.exports = {
