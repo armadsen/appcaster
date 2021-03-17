ad7986a2b9132c75d43840c07b9d1307bf150662
Andrew Madsen <andrew@openreelsoftware.com>
2021-03-17 20:16:22 +0000

    Use heroku recommended code for configuring database

--- config/index.js
+++ config/index.js
@@ -1,29 +1,36 @@
-var fs = require('fs');
-var env = process.env.NODE_ENV || 'development';
-process.env.AC_GITHUB_USERS = process.env.AC_GITHUB_USERS || ['alexyoung'];
+var fs = require("fs");
+var env = process.env.NODE_ENV || "development";
+process.env.AC_GITHUB_USERS = process.env.AC_GITHUB_USERS || ["alexyoung"];
 
 function parseGitHubUsers(users) {
-  return users.split(',').map(function(username) {
+  return users.split(",").map(function (username) {
     return username.trim();
   });
 }
 
+var database = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
+
 // Use environmental variables by default
 module.exports = {
   rootUrl: process.env.AC_ROOT_URL,
-  database: process.env.AC_DATABASE_URL,
+  database: database,
   github: {
-    client_id: process.env.AC_GITHUB_CLIENT_ID || 'Not set',
-    callback: process.env.AC_GITHUB_CALLBACK_URL || 'Not set',
-    secret: process.env.AC_GITHUB_SECRET || 'Not set'
+    client_id: process.env.AC_GITHUB_CLIENT_ID || "Not set",
+    callback: process.env.AC_GITHUB_CALLBACK_URL || "Not set",
+    secret: process.env.AC_GITHUB_SECRET || "Not set",
   },
   users: {
-    github: parseGitHubUsers(process.env.AC_GITHUB_USERS)
-  }
+    github: parseGitHubUsers(process.env.AC_GITHUB_USERS),
+  },
 };
 
-if (fs.existsSync('config/' + env + '.js')) {
-  module.exports = require('./' + env);
+if (fs.existsSync("config/" + env + ".js")) {
+  module.exports = require("./" + env);
 }
 
 module.exports.parseGitHubUsers = parseGitHubUsers;
